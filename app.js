d95519bea3e7ab47346d68c6a3df5a53b4509b86
Juan Paulo Passi <passipaulo14@gmail.com>
2024-09-10 14:55:25 +0800

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ let gender = "male";
 
 let twitterhandle = "@paulo_passi0610";
 
-console.log("name");
-console.log("WhatDoYouWannaBecomeInYourLife");
-console.log("gender");
-console.log("Twitterhandle");
+console.log(name);
+console.log(WhatDoYouWannaBecomeInYourLife);
+console.log(gender);
+console.log(twitterhandle);
