ec89c14b422f274d8b96d53bae28814cd981c713
Juan Paulo Passi <passipaulo14@gmail.com>
2024-09-10 14:06:06 +0800

    Update and rename README.md to app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+let name = "Juan Paulo Passi";
+
+let WhatDoYouWannaBecomeInYourLife = "programmer";
+
+let gender = "male";
+
+let twitterhandle = "@paulo_passi0610";
+
+console.log("name");
+console.log("WhatDoYouWannaBecomeInYourLife");
+console.log("gender");
+console.log("Twitterhandle");
